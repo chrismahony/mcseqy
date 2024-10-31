0d30febd857cb5351c8b341c4f258a1007d71928
mahonyc <65592074+chrismahony@users.noreply.github.com>
2024-10-31 13:47:21 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+setup(
+    name='mcseqy',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'scanpy>=1.7'
+    ],
+    description='A package for processing single-cell RNA-seq analysis.',
+    author='Chris Mahony',
+    author_email='your_email@example.com',
+    url='TBC', 
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
